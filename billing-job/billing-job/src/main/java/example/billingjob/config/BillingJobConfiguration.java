5e7195ae287c5bca25e742d73c0fb22a8f99b4da
Hmayda <abdoessamadhmayda@gmail.com>
2025-02-05 13:48:31 +0100

    bootstrapping the project

---
+++ billing-job/billing-job/src/main/java/example/billingjob/config/BillingJobConfiguration.java
@@ -0,0 +1,8 @@
+package example.billingjob.config;
+
+import org.springframework.context.annotation.Configuration;
+
+//This class is a placeholder for Spring Batch related beans (Jobs, Steps, etc)
+@Configuration
+public class BillingJobConfiguration {
+}
