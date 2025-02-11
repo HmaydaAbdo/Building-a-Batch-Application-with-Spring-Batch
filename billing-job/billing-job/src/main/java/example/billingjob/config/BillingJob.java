af20e351de82918be7125983af5711277f3aabd2
Hmayda <abdoessamadhmayda@gmail.com>
2025-02-11 12:52:08 +0100

    process a csv file

--- billing-job/billing-job/src/main/java/example/billingjob/config/BillingJob.java
+++ billing-job/billing-job/src/main/java/example/billingjob/config/BillingJob.java
@@ -1,9 +1,6 @@
 package example.billingjob.config;
 
-import org.springframework.batch.core.BatchStatus;
-import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.repository.JobRepository;
 
 public class BillingJob implements Job {
@@ -19,11 +16,14 @@ public class BillingJob implements Job {
         return "BillingJob";
     }
 
-    // status update
+
+    // creating jobInstances  by passing job params
 
     @Override
     public void execute(JobExecution execution) {
-        System.out.println("processing billing information");
+        JobParameters jobParameters = execution.getJobParameters();
+        String inputFile = jobParameters.getString("input.file");
+        System.out.println("processing billing information from file " + inputFile);
         execution.setStatus(BatchStatus.COMPLETED);
         execution.setExitStatus(ExitStatus.COMPLETED);
         this.jobRepository.update(execution);
@@ -43,5 +43,16 @@ public class BillingJob implements Job {
 //            this.jobRepository.update(execution);
 //        }
 //    }
+
+    // status update
+
+//    @Override
+//    public void execute(JobExecution execution) {
+//        System.out.println("processing billing information");
+//        execution.setStatus(BatchStatus.COMPLETED);
+//        execution.setExitStatus(ExitStatus.COMPLETED);
+//        this.jobRepository.update(execution);
+//    }
+
 
 }
